b1d1627ffb37dec7e8f5e7f731480a799f720ace
Luke <lukepensiero@gmail.com>
2016-05-20 01:32:40 +0000

    changed loop into list comprehension

--- language_detector/main.py
+++ language_detector/main.py
@@ -7,21 +7,20 @@ from .languages import LANGUAGES
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
     # stores the number of matching words per language
-    counters = []
-    
-    for language in languages:
-        i = 0
-        
-        for word in text.split():
-            
-            word = word.lower()
-            
-            if word in language['common_words']:
-                i += 1
-                
-        counters.append(i)
-  
+    counters = [count_words(language['common_words'], text) for language in languages]
+
     # finds the index of the largest word count
     word_count_index = counters.index(max(counters))
 
     return languages[word_count_index]['name']
+
+def count_words(common_words, text):
+    """checks how many words in text are in the common words given"""
+    i = 0
+    for word in text.split():
+            
+        word = word.lower()
+            
+        if word in common_words:
+            i += 1
+    return i
