c7fb808245e25115f75473a9e7af363655f3df44
Luke <lukepensiero@gmail.com>
2016-05-20 00:04:59 +0000

    Solution implemented and .gitignore updated

--- language_detector/main.py
+++ language_detector/main.py
@@ -5,5 +5,18 @@ from .languages import LANGUAGES
 
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    # stores the number of matching words per language
+    counters = []
+    
+    for language in languages:
+        i = 0
+        
+        for word in text.split():
+            
+            if word in language['common_words']:
+                i += 1
+        counters.append(i)
+  
+    word_count_index = counters.index(max(counters))
+
+    return languages[word_count_index]['name']
